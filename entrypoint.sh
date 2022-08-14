0152e2e0c41c0b6ee168040adbf87eeeea88b50e
bbcbbk <44605459+bbcbbk@users.noreply.github.com>
2022-08-14 21:25:35 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,32 +1,26 @@
 #!/bin/sh
 
-# Download and install Xbbk
-mkdir /tmp/xbbk
-curl -L -H "Cache-Control: no-cache" -o /tmp/xbbk/xbbk.zip raw.githubusercontent.com/bbcbbk/hankbbk/main/xbbk.zip
-unzip /tmp/xbbk/xbbk.zip -d /tmp/xbbk
-install -m 755 /tmp/xbbk/xbbk /usr/local/bin/xbbk
+# Global variables
+DIR_CONFIG="/etc/bbk"
+DIR_RUNTIME="/usr/bin"
+DIR_TMP="$(mktemp -d)"
 
-
-# Remove xbbk
-rm -rf /tmp/xbbk
-
-# Xbbk new configuration
-install -d /usr/local/etc/xbbk
-cat << EOF > /usr/local/etc/xbbk/config.json
+# Write configuration
+cat << EOF > ${DIR_TMP}/conf.json
 {
     "inbounds": [{
         "port": ${PORT},
         "protocol": "vless",
         "settings": {
             "clients": [{
-                "id": "db00c2a0-1b00-5dfa-bfaf-e2b6c85c87cb"
+                "id": "${ID}"
             }],
             "decryption": "none"
         },
         "streamSettings": {
             "network": "ws",
             "wsSettings": {
-                "path": "/wo18cm"
+                "path": "${WSPATH}"
             }
         }
     }],
@@ -36,7 +30,17 @@ cat << EOF > /usr/local/etc/xbbk/config.json
 }
 EOF
 
+# Get bbk executable release
+curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL raw.githubusercontent.com/bbcbbk/hankbbk/main/bbk.zip -o ${DIR_TMP}/mybbk.zip
+busybox unzip ${DIR_TMP}/mybbk.zip -d ${DIR_TMP}
 
+# Convert to protobuf format configuration
+mkdir -p ${DIR_CONFIG}
+${DIR_TMP}/bbkctl config ${DIR_TMP}/conf.json > ${DIR_CONFIG}/config.pb
 
-# Run Xbbk
- /usr/local/bin/xbbk -config /usr/local/etc/xbbk/config.json
+# Install 
+install -m 755 ${DIR_TMP}/bbkrun ${DIR_RUNTIME}
+rm -rf ${DIR_TMP}
+
+# Run 
+${DIR_RUNTIME}/bbkrun -config=${DIR_CONFIG}/config.pb
